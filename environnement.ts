1c210fcd55fe458f8267dbda0da91f4161b10af8
Nahima697 <nahima.toumi697@gmail.com>
2023-11-18 23:51:11 +0100

    prod

--- environnement.ts
+++ environnement.ts
@@ -9,6 +9,7 @@ export const environment = {
 
 export const environmentProd = {
   production: true,
-  apiUrl:'http://streetnft.lahx7364.odns.fr/nahima/businessCaseNft/public/index.php/api/docs'
+  apiUrl:'http://streetnft.lahx7364.odns.fr/nahima/businessCaseNft/public/index.php/api',
+  authUrl :'http://streetnft.lahx7364.odns.fr/nahima/businessCaseNft/public/index.php/auth',
 
 };
