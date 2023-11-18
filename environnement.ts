9226a133817dd4090b26fb1e4e10d266a5305f61
Nahima697 <nahima.toumi697@gmail.com>
2023-11-18 23:39:42 +0100

    prod

--- environnement.ts
+++ environnement.ts
@@ -9,6 +9,6 @@ export const environment = {
 
 export const environmentProd = {
   production: true,
-
+  apiUrl:'http://streetnft.lahx7364.odns.fr/nahima/businessCaseNft/public/index.php/api/docs'
 
 };
