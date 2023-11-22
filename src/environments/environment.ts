0f361542a6a549c55c1fe28b60254020f7c21207
Nahima697 <nahima.toumi697@gmail.com>
2023-11-23 00:20:28 +0100

    nettoyage

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
 
-export const environment = {
-  production: false,
-  apiUrl: 'https://127.0.0.1:8000/api',
-  authUrl :'https://127.0.0.1:8000/auth',
-  url: 'https://streetnft.lahx7364.odns.fr/nahima/businessCaseNft/public/',
-
-};
 // export const environment = {
-//   production: true,
-//   apiUrl:'http://streetnft.lahx7364.odns.fr/nahima/businessCaseNft/public/index.php/api',
-//   authUrl :'http://streetnft.lahx7364.odns.fr/nahima/businessCaseNft/public/index.php/auth',
+//   production: false,
+//   apiUrl: 'https://127.0.0.1:8000/api',
+//   authUrl :'https://127.0.0.1:8000/auth',
+//   url: 'https://streetnft.lahx7364.odns.fr/nahima/businessCaseNft/public/',
 
 // };
+export const environment = {
+  production: true,
+  apiUrl:'http://streetnft.lahx7364.odns.fr/nahima/businessCaseNft/public/index.php/api',
+  authUrl :'http://streetnft.lahx7364.odns.fr/nahima/businessCaseNft/public/index.php/auth',
+
+};
